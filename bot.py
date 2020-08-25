8004666aae97082518eea01582d3dd8624cf8647
Christopher Sien <imchrissien@gmail.com>
2020-08-24 17:33:04 -0700

    adding missing line to on_message

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord
 import config
+from discord.ext import commands
 from config import token
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '$')
 
 @client.event
 async def on_ready():
@@ -12,5 +13,10 @@ async def on_ready():
 async def on_message(message):
     if 'creeper' in message.content.lower():
         await message.channel.send('AW MAN')
+    await client.process_commands(message)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
 
 client.run(token)
