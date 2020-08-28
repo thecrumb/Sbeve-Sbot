248417838f3aa3d9bf1d7d06da7015bbc44d202f
Christopher Sien <imchrissien@gmail.com>
2020-08-27 19:48:58 -0700

    can play music with url

--- cogs/music.py
+++ cogs/music.py
@@ -1,12 +1,54 @@
+import asyncio
 import discord
+import youtube_dl
 from discord.ext import commands
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        return cls(discord.FFmpegPCMAudio(data['url'], **ffmpeg_options), data=data)
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    # @commands.command()
-    # async def play(self, ctx):
 
     @commands.command()
     async def join(self, ctx):
@@ -19,10 +61,19 @@ class Music(commands.Cog):
         await channel.connect()
 
     @commands.command()
+    async def play(self, ctx, *, url):
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send(f'Now playing: {player.title}')
+
+    @commands.command()
     async def leave(self, ctx):
         """Disconnects the bot from voice"""
         await ctx.voice_client.disconnect()
-    
+
     @commands.command()
     async def lyrics(self, ctx):
         embed1 = discord.Embed(
